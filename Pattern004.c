49b91c6faa5f26c03bc0d7c858ff118987d36f48
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-10-30 22:40:37 +0530

    Update Pattern004.c

--- Pattern004.c
+++ Pattern004.c
@@ -1,9 +1,9 @@
 /* Accept number from user and display below pattern
 		Input:	7846
 		Output:	7	6	5	4	3	2	1
-					8	7	6	5	4	3	2	1
-					4	3	2	1
-					6	5	4	3	2	1						*/
+			8	7	6	5	4	3	2	1
+			4	3	2	1
+			6	5	4	3	2	1						*/
 					
 #include<stdio.h>
 
